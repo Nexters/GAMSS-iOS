cbca36582afd0e41012155a73a2991aa6eb55c66
dlrjswns <leekyunjun@gmail.com>
2026-09-23 10:27:55 +0900

    [ADD] 닉네임 길이 제한관련 Entity 모델 추가

---
+++ GAMSS/Sources/Domain/Entity/NicknamePolicy.swift
@@ -0,0 +1,13 @@
+//
+//  NicknamePolicy.swift
+//  GAMSS
+//
+//  Created by 이건준 on 9/23/26.
+//
+
+import Foundation
+
+enum NicknamePolicy {
+    static let minimumLength = 2
+    static let maximumLength = 10
+}
